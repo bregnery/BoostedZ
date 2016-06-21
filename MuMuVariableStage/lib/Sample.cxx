bd616b86053b3a27fc621ec68b747c925f24cb50
bregnery <bregnery@ufl.edu>
2016-06-21 14:26:58 -0400

    Andrew's Working DiMuAnalysis before updates

--- MuMuVariableStage/lib/Sample.cxx
+++ MuMuVariableStage/lib/Sample.cxx
@@ -18,9 +18,9 @@
 
 Sample::Sample() 
 {
-    lumiWeights_ = 0;
-    xsec_ = -999; 
-    lumi_ = -999;
+    lumiWeights = 0;
+    xsec = -999; 
+    lumi = -999;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -29,16 +29,16 @@ Sample::Sample()
 
 Sample::Sample(TString ifilename, TString iname)
 {
-    filename_ = ifilename;
-    name_ = iname;
-    treename_ = TString("dimuons/tree");
-    file_ = new TFile(filename);
-    tree_ = (TTree*)file->Get(treename);
-    N_ = tree->GetEntries();
+    filename = ifilename;
+    name = iname;
+    treename = TString("dimuons/tree");
+    file = new TFile(filename);
+    tree = (TTree*)file->Get(treename);
+    N = tree->GetEntries();
 
-    lumiWeights_ = 0;
-    xsec_ = -999; 
-    lumi_ = -999;
+    lumiWeights = 0;
+    xsec = -999; 
+    lumi = -999;
     
     setBranchAddresses();
     calculateNoriginal();
@@ -50,17 +50,17 @@ Sample::Sample(TString ifilename, TString iname)
 
 Sample::Sample(TString ifilename, TString iname, TString isampleType)
 {
-    filename_ = ifilename;
-    name_ = iname;
-    sampleType_ = isampleType;
-    treename_ = TString("dimuons/tree");
-    file_ = new TFile(filename);
-    tree_ = (TTree*)file->Get(treename);
-    N_ = tree->GetEntries();
+    filename = ifilename;
+    name = iname;
+    sampleType = isampleType;
+    treename = TString("dimuons/tree");
+    file = new TFile(filename);
+    tree = (TTree*)file->Get(treename);
+    N = tree->GetEntries();
 
-    lumiWeights_ = 0;
-    xsec_ = -999; 
-    lumi_ = -999;
+    lumiWeights = 0;
+    xsec = -999; 
+    lumi = -999;
     
     setBranchAddresses();
     calculateNoriginal();
@@ -73,13 +73,13 @@ Sample::Sample(TString ifilename, TString iname, TString isampleType)
 Sample::~Sample() {
   // free pointed to memory!
   if (tree != 0) {
-    delete tree_;
+    delete tree;
   }
   if (file !=0) {
-    delete file_;
+    delete file;
   }
   if (lumiWeights !=0) {
-    delete lumiWeights_;
+    delete lumiWeights;
   }
 }
 
@@ -89,19 +89,19 @@ Sample::~Sample() {
 
 void Sample::setBranchAddresses()
 {
-    tree_->SetBranchAddress("recoCandMass", &vars.recoCandMass);
-    tree_->SetBranchAddress("nPU", &vars.nPU);
-    tree_->SetBranchAddress("recoCandPt", &vars.recoCandPt);
-    tree_->SetBranchAddress("vertexInfo", &vars.vertices);
-    tree_->SetBranchAddress("eventInfo", &vars.eventInfo);
-    tree_->SetBranchAddress("reco1", &vars.reco1);
-    tree_->SetBranchAddress("reco2", &vars.reco2);
-    tree_->SetBranchAddress("genWeight", &vars.genWeight);
-    tree_->SetBranchAddress("pfJets", &vars.jets);
-    tree_->SetBranchAddress("met", &vars.met);
-    tree_->SetBranchAddress("recoCandMassPF", &vars.recoCandMassPF);
-    tree_->SetBranchAddress("recoCandPtPF", &vars.recoCandPtPF);
-    tree_->SetBranchAddress("rho", &vars.rho);
+    tree->SetBranchAddress("recoCandMass", &vars.recoCandMass);
+    tree->SetBranchAddress("nPU", &vars.nPU);
+    tree->SetBranchAddress("recoCandPt", &vars.recoCandPt);
+    tree->SetBranchAddress("vertexInfo", &vars.vertices);
+    tree->SetBranchAddress("eventInfo", &vars.eventInfo);
+    tree->SetBranchAddress("reco1", &vars.reco1);
+    tree->SetBranchAddress("reco2", &vars.reco2);
+    tree->SetBranchAddress("genWeight", &vars.genWeight);
+    tree->SetBranchAddress("pfJets", &vars.jets);
+    tree->SetBranchAddress("met", &vars.met);
+    tree->SetBranchAddress("recoCandMassPF", &vars.recoCandMassPF);
+    tree->SetBranchAddress("recoCandPtPF", &vars.recoCandPtPF);
+    tree->SetBranchAddress("rho", &vars.rho);
 }
 ///////////////////////////////////////////////////////////////////////////////
 //-----------------------------------------------------------------------------
@@ -111,17 +111,17 @@ void Sample::calculateNoriginal()
 {
 // Calculate the number of original events using the meta data tree
 // Calculate the weighted number of original events as well
-    TTree* metadata = (TTree*)file_->Get("dimuons/metadata");
+    TTree* metadata = (TTree*)file->Get("dimuons/metadata");
     metadata->Draw("sumEventWeights>>eweights_"+name);
     TH1F* sumEventWeightsHist = (TH1F*) gDirectory->Get("eweights_"+name); 
 
     // There are many ttrees combined so the histogram has a numEvents entry for each
     // file. The total number is equal to the average number times the total number of entries.
-    nOriginalWeighted_ = sumEventWeightsHist->GetEntries()*sumEventWeightsHist->GetMean();
+    nOriginalWeighted = sumEventWeightsHist->GetEntries()*sumEventWeightsHist->GetMean();
 
     metadata->Draw("originalNumEvents>>nevents_"+name);
     TH1F* nEventsHist = (TH1F*) gDirectory->Get("nevents_"+name); 
-    nOriginal_ = nEventsHist->GetEntries()*nEventsHist->GetMean();
+    nOriginal = nEventsHist->GetEntries()*nEventsHist->GetMean();
     if(metadata !=0) delete metadata;
 }
 
@@ -131,7 +131,7 @@ void Sample::calculateNoriginal()
 
 int Sample::getEntry(int i)
 {
-    tree_->GetEntry(i);
+    tree->GetEntry(i);
     return i;
 }
 
@@ -154,8 +154,8 @@ float Sample::getWeight()
 {
 // Assumes getEntry has already been called to load the appropriate values.
 // Gets the weight for the histogram depending on the sample type 
-    if(sampleType_.Contains("data")) return 1.0;
-    else return 1.0*vars_.genWeight*lumiWeights->weight(vars.nPU);
+    if(sampleType.Contains("data")) return 1.0;
+    else return 1.0*vars.genWeight*lumiWeights->weight(vars.nPU);
     //else return 1.0*vars.genWeight;
 }
 
@@ -168,7 +168,7 @@ float Sample::getScaleFactor(float luminosity)
 // Scale the MC histograms based upon the data luminosity, the number of events
 // that the CMSSW analyzer looked at, and the xsec for the process
     if(sampleType.Contains("data")) return 1.0;
-    else return luminosity*xsec_/nOriginalWeighted_;
+    else return luminosity*xsec/nOriginalWeighted;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -180,5 +180,5 @@ float Sample::getScaleFactor(float luminosity, float reductionFactor)
 // Scale the MC histograms based upon the data luminosity, the number of events
 // that the CMSSW analyzer looked at, and the xsec for the process
     if(sampleType.Contains("data")) return 1.0;
-    else return luminosity*xsec_/nOriginalWeighted_;
+    else return luminosity*xsec/nOriginalWeighted;
 }
--- MuMuVariableStage/lib/Sample.h
+++ MuMuVariableStage/lib/Sample.h
@@ -14,46 +14,45 @@
 
 class Sample
 {
-    private:
-        TString name_;
-        TString filename_;
-        TString treename_;
-        TTree* tree_;
-        reweight::LumiReWeighting* lumiWeights_;  // Information for pileup reweighting 
-
-        TString dir_;           // DAS directory
-        TString pileupfile_;    // used for pile up reweighting through lumiWeights
-        TString sampleType_;    // "data", "signal", "background"
-
-        int plotColor_;         // the color used when plotting the sample
-        int nOriginal_;         // the number of events run over to get this sample
-        int nOriginalWeighted_; // the number of original events run over to get this sample accounting for genWeights
-        int nEvents_;           // the number of events in the sample
-
-        float xsec_;            // xsec in pb
-        float lumi_;            // the luminosity of the data or effective luminosity of MC
-
-        VarSet vars_;           // all of the variables from the ttree
-
     public:
         Sample();
         Sample(TString infilename, TString name);
         Sample(TString infilename, TString name, TString sampleType);
         ~Sample();
 
+        TString name;
+        TString filename;
+        TString treename;
+        TTree* tree;
+        reweight::LumiReWeighting* lumiWeights;  // Information for pileup reweighting 
+
+        TString dir;           // DAS directory
+        TString pileupfile;    // used for pile up reweighting through lumiWeights
+        TString sampleType;    // "data", "signal", "background"
+
+        int plotColor;         // the color used when plotting the sample
+        int nOriginal;         // the number of events run over to get this sample
+        int nOriginalWeighted; // the number of original events run over to get this sample accounting for genWeights
+        int N;                 // the number of events in the sample
+
+        float xsec;            // xsec in pb
+        float lumi;            // the luminosity of the data or effective luminosity of MC
+
+        VarSet vars;           // all of the variables from the ttree
+
         int getEntry(int i);                    // load the ith event from the ttree into vars
         int getEntry(int i, TEntryList* list);  // load ith event from the list into vars
                                                 // the ith event in the list maps to the jth tree entry
-                                                
+
         void calculateNoriginal(); // calculate nOriginal and nOriginalWeighted
         void setBranchAddresses(); // link the values in the tree to vars
         float getWeight();         // get the weight for the histogram based upon the pileup weight and the MC gen weight
         float getScaleFactor(float luminosity); // get the scale factor for the MC histogram based upon the number of events, the data luminosity, and the xsec for the process 
         float getScaleFactor(float luminosity, float reductionFactor); // get the scale factor for the MC histogram based upon the number of events, the data luminosity, and the xsec for the process 
-         
-     protected:
+
+    protected:
         TFile* file;           // the file with the ttree
-                      
+
 };
 
-#endif                                              //                                                     #endif
+#endif
